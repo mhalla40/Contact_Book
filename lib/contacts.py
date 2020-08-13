4660760c2bd7a8853e89adec508fb7237c62016f
Michael Hall <mhalla40@gmail.com>
2020-08-13 11:16:39 -0400

    created more contacts

--- lib/contacts.py
+++ lib/contacts.py
@@ -23,5 +23,16 @@ db.create_tables([Contact])
 
 michael = Contact(first_name='Michael', last_name='Hall',
                   phone='555-5555', birthday=date(1985, 2, 20))
-
 michael.save()
+
+kim = Contact(first_name='Kim', last_name='Locks',
+              phone='510-5555', birthday=date(1987, 3, 15))
+kim.save()
+
+steve = Contact(first_name='Steve', last_name='McQueen',
+                phone='525-5555', birthday=date(1985, 8, 30))
+steve.save()
+
+lisa = Contact(first_name='Lisa', last_name='Bonet',
+               phone='545-5555', birthday=date(1986, 1, 17))
+lisa.save()
