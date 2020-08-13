d7506c08831f74a949d662cca9f8357369568099
Michael Hall <mhalla40@gmail.com>
2020-08-13 09:13:48 -0400

    created a Contact table

--- lib/contacts.py
+++ lib/contacts.py
@@ -1,6 +1,22 @@
 from peewee import *
+from datetime import date
 
 db = PostgresqlDatabase('book', user='postgres', password='',
                         host='localhost', port=5432)
 
 db.connect()
+
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+
+class Contact(BaseModel):
+    first_name = CharField()
+    last_name = CharField()
+    phone = CharField()
+    birthday = DateField()
+
+
+db.create_tables([Contact])
